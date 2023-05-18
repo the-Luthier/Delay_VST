f24c0ac6e2d49ad8e348b448ffde1f0e9407921f
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-18 17:43:32 +0300

    1234

--- onurkaroffdelayvst/Program.cs
+++ onurkaroffdelayvst/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using NAudio.Wave;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+namespace OnurKaroffDelay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new AudioFileReader("test.wav"))
+            {
+                var delay = new DelayEffect(reader, 1000, 500, 0.5f, 0.8f);
+                WaveFileWriter.CreateWaveFile16("test_delay.wav", delay);
+            }
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
+
--- onurkaroffdelayvst/delay_design.cs
+++ onurkaroffdelayvst/delay_design.cs
@@ -4,43 +4,45 @@ using NAudio.Wave;
 namespace OnurKaroffDelay
 {
     public class DelayEffect : ISampleProvider
-{
-    private ISampleProvider source;
-    private float[] delayBuffer;
-    private int delayBufferIndex;
-
-    public float DelayTime { get; set; }
-    public float Feedback { get; set; }
-    public float WetDryMix { get; set; }
-
-    public DelayEffect(ISampleProvider source, int maxDelayMilliseconds, float delayTime, float feedback, float wetDryMix)
     {
-        this.source = source;
-        delayBuffer = new float[maxDelayMilliseconds * source.WaveFormat.SampleRate / 1000];
-        DelayTime = delayTime;
-        Feedback = feedback;
-        WetDryMix = wetDryMix;
-    }
+        private ISampleProvider source;
+        private float[] delayBuffer;
+        private int delayBufferIndex;
 
-    public WaveFormat WaveFormat => source.WaveFormat;
+        public float DelayTime { get; set; }
+        public float Feedback { get; set; }
+        public float WetDryMix { get; set; }
 
-    public int Read(float[] buffer, int offset, int count)
-    {
-        int sourceSamplesRead = source.Read(buffer, offset, count);
-
-        for (int n = 0; n < sourceSamplesRead; n++)
+        public DelayEffect(ISampleProvider source, int maxDelayMilliseconds, float delayTime, float feedback, float wetDryMix)
         {
-            var readIndex = (delayBufferIndex + delayBuffer.Length - (int)(DelayTime * source.WaveFormat.SampleRate / 1000)) % delayBuffer.Length;
-            var delayedSample = delayBuffer[readIndex];
-            var inputSample = buffer[offset + n];
-            delayBuffer[delayBufferIndex] = inputSample + delayedSample * Feedback;
-            buffer[offset + n] = inputSample * (1 - WetDryMix) + delayedSample * WetDryMix;
-            delayBufferIndex = (delayBufferIndex + 1) % delayBuffer.Length;
+            this.source = source;
+            delayBuffer = new float[maxDelayMilliseconds * source.WaveFormat.SampleRate / 1000];
+            DelayTime = delayTime;
+            Feedback = feedback;
+            WetDryMix = wetDryMix;
         }
 
-        return sourceSamplesRead;
+        public WaveFormat WaveFormat => source.WaveFormat;
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            int sourceSamplesRead = source.Read(buffer, offset, count);
+
+            for (int n = 0; n < sourceSamplesRead; n++)
+            {
+                var readIndex = (delayBufferIndex + delayBuffer.Length - (int)(DelayTime * source.WaveFormat.SampleRate / 1000)) % delayBuffer.Length;
+                var delayedSample = delayBuffer[readIndex];
+                var inputSample = buffer[offset + n];
+
+                delayBuffer[delayBufferIndex] = inputSample + delayedSample * Feedback;
+                buffer[offset + n] = inputSample * (1 - WetDryMix) + delayedSample * WetDryMix;
+
+                delayBufferIndex = (delayBufferIndex + 1) % delayBuffer.Length;
+            }
+
+            return sourceSamplesRead;
+        }
     }
 }
 
-}
 
